8357aa5ab76106dc5e12ce1e8cd647b2ac9c9d19
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2025-04-09 23:22:21 +0300

    did some alterations based on theprimeagen configs

--- nvim/.config/nvim/lua/moeajam/plugins/telescope.lua
+++ nvim/.config/nvim/lua/moeajam/plugins/telescope.lua
@@ -1,38 +1,68 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+    "nvim-telescope/telescope.nvim",
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        --mappings = {
-          --i = {
-          --  ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-          --  ["<C-j>"] = actions.move_selection_next, -- move to next result
-          --  ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          --},
-        --},
-      },
-    })
+    tag = "0.1.5",
 
-    telescope.load_extension("fzf")
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    config = function()
+        require('telescope').setup({})
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-  end,
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>fw', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    end
 }
+
+-- return {
+--   "nvim-telescope/telescope.nvim",
+--   branch = "0.1.x",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+--     "nvim-tree/nvim-web-devicons",
+--     "folke/todo-comments.nvim",
+--   },
+--   config = function()
+--     local telescope = require("telescope")
+--     local actions = require("telescope.actions")
+--
+--     telescope.setup({
+--       defaults = {
+--         path_display = { "smart" },
+--         --mappings = {
+--           --i = {
+--           --  ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+--           --  ["<C-j>"] = actions.move_selection_next, -- move to next result
+--           --  ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+--           --},
+--         --},
+--       },
+--     })
+--
+--     telescope.load_extension("fzf")
+--
+--     -- set keymaps
+--     local keymap = vim.keymap -- for conciseness
+--
+--     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+--     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+--     keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+--     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+--     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+--   end,
+-- }
