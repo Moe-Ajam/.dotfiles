31b5c983b6e6e25eeda7786b21759c79eb09e428
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2025-04-19 16:15:53 +0300

    Fixed the lsp settings to be more compatible with my old keymaps

--- nvim/.config/nvim/lua/moeajam/plugins/cmp.lua
+++ nvim/.config/nvim/lua/moeajam/plugins/cmp.lua
@@ -35,13 +35,11 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-b>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<C-y>"] = cmp.mapping.confirm({ select = false }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
