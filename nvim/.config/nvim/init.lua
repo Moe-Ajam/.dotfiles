f280564f67922e7124324f83f666a3e4bddc35c1
Moe-Ajam <mahmoud.othman.ajam@gmail.com>
2025-04-26 17:28:31 +0300

    Rewriting my nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,23 @@
-require("moeajam.core")
-require("moeajam.lazy")
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+
+-- Lets you paste whatever you have in your global clipboard with "P"
+vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.keymap.set("n","<space>e", ":Explore<CR>")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
